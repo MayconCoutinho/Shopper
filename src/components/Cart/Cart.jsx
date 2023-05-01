1609419b9a661e329258da52f450677fd18e247d
MayconCoutinho <mayconcoutinhoart@gmail.com>
2023-05-01 00:16:22 -0300

    corrigindo erros de url

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,117 +1,141 @@
-import React from 'react';
-import { 
-    CartCss,
-    ContainerCss,
-    NameCss,
-    PriceCss,
-    StockCss,
-    ButtonAddCart,
-    InputCss,
-    ContainerGridCss,
-    ButtonDeleteCSS,
-    ButtonPutCSS,
-    CartBlockCss
-} from './styled.jsx';
-import { useContext } from 'react'
-import {GlobalContext} from "../../global/context/useContext.js"
+import React from "react";
+import {
+  CartCss,
+  ContainerCss,
+  NameCss,
+  PriceCss,
+  StockCss,
+  ButtonAddCart,
+  InputCss,
+  ContainerGridCss,
+  ButtonDeleteCSS,
+  ButtonPutCSS,
+  CartBlockCss,
+} from "./styled.jsx";
+import { useContext } from "react";
+import { GlobalContext } from "../../global/context/useContext.js";
 import { TiPlus } from "react-icons/ti";
-import { MdRemoveShoppingCart, MdAddShoppingCart } from 'react-icons/md';
-import { useForm } from '../../hooks/useForm.js';
-import { deleteProductsUser, postUserProducts, putUpProductQuantity } from '../../services/ApiShopper.jsx';
+import { MdRemoveShoppingCart, MdAddShoppingCart } from "react-icons/md";
+import { useForm } from "../../hooks/useForm.js";
+import {
+  deleteProductsUser,
+  postUserProducts,
+  putUpProductQuantity,
+} from "../../services/ApiShopper.jsx";
 
 export const Cart = () => {
-    const { products } = useContext(GlobalContext)
-    const {user,cartItem, setTimesAddedProducts, timesAddedProducts} = useContext(GlobalContext)
+  const { products } = useContext(GlobalContext);
+  const { user, cartItem, setTimesAddedProducts, timesAddedProducts } =
+    useContext(GlobalContext);
 
-    const {formValues, onChange} = useForm([{
-        id:"",
-        quantity:""
-    }])    
-    const PutQuantity = (id, totalQuantity) => {
-
-        if( formValues.quantity <= totalQuantity){
-
-        putUpProductQuantity(id,formValues.quantity)
-        setTimeout(() => {
-            alert("Produto mudado com sucesso")
-            setTimesAddedProducts(timesAddedProducts + 1)
-          }, "100")
-        } else {
-            alert("A quantidade de produto é maior do que a de estoque")
+  const { formValues, onChange } = useForm([
+    {
+      id: "",
+      quantity: "",
+    },
+  ]);
+  const PutQuantity = (id, totalQuantity) => {
+    if (formValues.quantity <= totalQuantity) {
+      putUpProductQuantity(id, formValues.quantity);
+      setTimeout(() => {
+        alert("Produto mudado com sucesso");
+        setTimesAddedProducts(timesAddedProducts + 1);
+      }, "100");
+    } else {
+      alert("A quantidade de produto é maior do que a de estoque");
+    }
+  };
+  const DeleteProduct = (id) => {
+    deleteProductsUser(id, user);
+    setTimeout(() => {
+      setTimesAddedProducts(timesAddedProducts + 1);
+    }, "100");
+  };
+  const ItemQuantity = (id) => {
+    const result =
+      cartItem &&
+      cartItem?.filter((item) => {
+        if (item.id_product == id) {
+          if (item.quantity > 0) {
+            return item.quantity;
+          }
         }
-        
-    }
-    const DeleteProduct = (id) => {
-        deleteProductsUser(id)
-        setTimeout(() => {
-            setTimesAddedProducts(timesAddedProducts + 1)
-          }, "100")
-    }
-    const ItemQuantity = (id) => {
-        const result = cartItem && cartItem?.filter((item) =>{
-             if(item.id_product == id){
-                 if(item.quantity > 0){
-                    return item.quantity
-                 } 
-             }})
-        return result[0].quantity
-    }
-    const OneItemAdd = (id) => {
-        const result = cartItem && cartItem?.filter((item) =>{
-            if(item.id_product == id){
-                    return true
-            } 
-        })
-        return result.length
-    }
-    const AddProductCart = (idProduct) => {
-        postUserProducts(idProduct,user)
-        setTimeout(() => {
-            setTimesAddedProducts(timesAddedProducts + 1)
-          }, "100")
-    }
-    return (
-        <ContainerCss>            
-            {
-            products && products.map((item) => {
-                return(
-                    <CartCss key={item.id}> 
-                     <CartBlockCss key={item.id} variant={ item.qty_stock === 0}>
-                        <p> Produto fora de estoque </p>    
-                    </CartBlockCss> 
-                        <NameCss> {item.name} </NameCss> 
-                        <PriceCss> {item.price.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })} </PriceCss>             
-                        { OneItemAdd(item.id) === 0 ? (<ButtonAddCart onClickCapture={() => AddProductCart(item.id)}><TiPlus/> Adicionar </ButtonAddCart>)
-                        :
-                        (
-                         <>    
-                        <ContainerGridCss>
-                        <ButtonPutCSS>
-                         <button onClick={() => PutQuantity(item.id, item.qty_stock)}> <MdAddShoppingCart/> </button>  
-                         </ButtonPutCSS>                                       
-                            <InputCss> 
-                                <input
-                                    type={"number"}
-                                    placeholder={ItemQuantity(item.id) }
-                                    name={"quantity"}
-                                    onChange={onChange}
-                                    required
-                                    min="1"
-                                    max={item.qty_stock}
-                                    value={formValues.name}
-                                    ></input>
-                              </InputCss>
-                            <ButtonDeleteCSS> 
-                                <button onClick={() => DeleteProduct(item.id)}> <MdRemoveShoppingCart/></button> 
-                            </ButtonDeleteCSS>
-                        </ContainerGridCss>      
-                            </>   
-                            )}
-                        <StockCss> {item.qty_stock} </StockCss> 
-                    </CartCss>
-                    )
-                })
-            }
-        </ContainerCss> 
-    )
-}
+      });
+    return result[0].quantity;
+  };
+  const OneItemAdd = (id) => {
+    const result =
+      cartItem &&
+      cartItem?.filter((item) => {
+        if (item.id_product == id) {
+          return true;
+        }
+      });
+    return result.length;
+  };
+  const AddProductCart = (idProduct) => {
+    postUserProducts(idProduct, user);
+    setTimeout(() => {
+      setTimesAddedProducts(timesAddedProducts + 1);
+    }, "100");
+  };
+  return (
+    <ContainerCss>
+      {products &&
+        products.map((item) => {
+          return (
+            <CartCss key={item.id}>
+              <CartBlockCss key={item.id} variant={item.qty_stock === 0}>
+                <p> Produto fora de estoque </p>
+              </CartBlockCss>
+              <NameCss> {item.name} </NameCss>
+              <PriceCss>
+                {" "}
+                {item.price.toLocaleString("pt-br", {
+                  style: "currency",
+                  currency: "BRL",
+                })}{" "}
+              </PriceCss>
+              {OneItemAdd(item.id) === 0 ? (
+                <ButtonAddCart onClickCapture={() => AddProductCart(item.id)}>
+                  <TiPlus /> Adicionar{" "}
+                </ButtonAddCart>
+              ) : (
+                <>
+                  <ContainerGridCss>
+                    <ButtonPutCSS>
+                      <button
+                        onClick={() => PutQuantity(item.id, item.qty_stock)}
+                      >
+                        {" "}
+                        <MdAddShoppingCart />{" "}
+                      </button>
+                    </ButtonPutCSS>
+                    <InputCss>
+                      <input
+                        type={"number"}
+                        placeholder={ItemQuantity(item.id)}
+                        name={"quantity"}
+                        onChange={onChange}
+                        required
+                        min="1"
+                        max={item.qty_stock}
+                        value={formValues.name}
+                      ></input>
+                    </InputCss>
+                    <ButtonDeleteCSS>
+                      <button onClick={() => DeleteProduct(item.id)}>
+                        {" "}
+                        <MdRemoveShoppingCart />
+                      </button>
+                    </ButtonDeleteCSS>
+                  </ContainerGridCss>
+                </>
+              )}
+              <StockCss> {item.qty_stock} </StockCss>
+            </CartCss>
+          );
+        })}
+    </ContainerCss>
+  );
+};
--- src/components/HeaderButtonCart/HeaderButtonCart.jsx
+++ src/components/HeaderButtonCart/HeaderButtonCart.jsx
@@ -1,128 +1,196 @@
-import React from 'react';
-import { 
-    HeaderButtonCartCss,
-    QuantityCartCss,
-    MenuCartCss,ArrowCss,
-    FinaliteBuy,
-    CartTotalPriceCss,
-    ItemCartCSS,
-    ScrollCSS,
-    EmptyCartCSS} from './styled.jsx';
-import { useState } from 'react';
-import { useContext } from 'react'
-import {GlobalContext} from "../../global/context/useContext.js"
+import React from "react";
+import {
+  HeaderButtonCartCss,
+  QuantityCartCss,
+  MenuCartCss,
+  ArrowCss,
+  FinaliteBuy,
+  CartTotalPriceCss,
+  ItemCartCSS,
+  ScrollCSS,
+  EmptyCartCSS,
+} from "./styled.jsx";
+import { useState } from "react";
+import { useContext } from "react";
+import { GlobalContext } from "../../global/context/useContext.js";
 import { TiShoppingCart } from "react-icons/ti";
-import { deleteProductsUser, putUpProductQuantity, putUpQtyStockQuantity } from '../../services/ApiShopper.jsx';
-import { useForm } from '../../hooks/useForm.js';
-import { MdRemoveShoppingCart, MdAddShoppingCart } from 'react-icons/md';
-import { InputCss,ContainerGridCss,ButtonDeleteCSS, ButtonPutCSS } from '../Cart/styled.jsx';
+import {
+  deleteProductsUser,
+  putUpProductQuantity,
+  putUpQtyStockQuantity,
+} from "../../services/ApiShopper.jsx";
+import { useForm } from "../../hooks/useForm.js";
+import { MdRemoveShoppingCart, MdAddShoppingCart } from "react-icons/md";
+import {
+  InputCss,
+  ContainerGridCss,
+  ButtonDeleteCSS,
+  ButtonPutCSS,
+} from "../Cart/styled.jsx";
 
 export const HeaderButtonCart = () => {
+  const [menuCart, setMenuCartTrue] = useState(false);
+  const {
+    user,
+    cartItemSum,
+    cartPriceSum,
+    cartItem,
+    setTimesAddedProducts,
+    timesAddedProducts,
+    products,
+  } = useContext(GlobalContext);
 
-const [menuCart, setMenuCartTrue] = useState(false)
-const { cartItemSum, cartPriceSum,cartItem, setTimesAddedProducts, timesAddedProducts,products} = useContext(GlobalContext)
-
-const {formValues, onChange} = useForm([{
-    id:"",
-    quantity:""
-}]) 
-const StockItemQuantity = (id) => {
+  const { formValues, onChange } = useForm([
+    {
+      id: "",
+      quantity: "",
+    },
+  ]);
+  const StockItemQuantity = (id) => {
     const result = products.filter((item) => {
-        return item.id === id
-    })
-    return result.map((item) => {return item.qty_stock})
-}
-const PutQuantity = (id) => {
-    putUpProductQuantity(id,formValues.quantity)
+      return item.id === id;
+    });
+    return result.map((item) => {
+      return item.qty_stock;
+    });
+  };
+  const PutQuantity = (id) => {
+    putUpProductQuantity(id, formValues.quantity);
     setTimeout(() => {
-        alert("Produto mudado com sucesso")
-        setTimesAddedProducts(timesAddedProducts + 1)
-      }, "100")
-}
-const DeleteProduct = (id) => {
-    deleteProductsUser(id)
+      alert("Produto mudado com sucesso");
+      setTimesAddedProducts(timesAddedProducts + 1);
+    }, "100");
+  };
+  const DeleteProduct = (id) => {
+    deleteProductsUser(id, user);
     setTimeout(() => {
-        setTimesAddedProducts(timesAddedProducts + 1)
-      }, "100")
-}
-const UserItemQuantity = (id) => {
-    const result = cartItem && cartItem?.filter((item) =>{
-         if(item.id_product == id){
-             if(item.quantity > 0){
-                return item.quantity
-             } 
-         }})
-    return result[0].quantity
-}
-const FinalizePurchase = () => {
-    cartItem.map((item)=> {
-        putUpQtyStockQuantity(item.id_product,( StockItemQuantity(item.id_product) - UserItemQuantity(item.id_product)))
-        DeleteProduct(item.id_product)
-    })
+      setTimesAddedProducts(timesAddedProducts + 1);
+    }, "100");
+  };
+  const UserItemQuantity = (id) => {
+    const result =
+      cartItem &&
+      cartItem?.filter((item) => {
+        if (item.id_product == id) {
+          if (item.quantity > 0) {
+            return item.quantity;
+          }
+        }
+      });
+    return result[0].quantity;
+  };
+  const FinalizePurchase = () => {
+    cartItem.map((item) => {
+      putUpQtyStockQuantity(
+        item.id_product,
+        StockItemQuantity(item.id_product) - UserItemQuantity(item.id_product)
+      );
+      DeleteProduct(item.id_product);
+    });
     setTimeout(() => {
-        setTimesAddedProducts(timesAddedProducts + 1)
-      }, "100")
-}
-const MenuCart = () => {
-    if(menuCart === false){
-        setMenuCartTrue(true)
+      setTimesAddedProducts(timesAddedProducts + 1);
+    }, "100");
+  };
+  const MenuCart = () => {
+    if (menuCart === false) {
+      setMenuCartTrue(true);
+    } else {
+      setMenuCartTrue(false);
     }
-    else{
-        setMenuCartTrue(false)
-    }
-}
-    return (
-        <>
-            <HeaderButtonCartCss onClick={() => MenuCart()} > 
-                <QuantityCartCss> {cartPriceSum > 0 ? cartItemSum : 0 } </QuantityCartCss><TiShoppingCart/> 
-                <CartTotalPriceCss> {cartPriceSum && cartPriceSum.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }) || 0 } </CartTotalPriceCss>
-            </HeaderButtonCartCss>
-            <MenuCartCss variant={ menuCart === true}> 
-            <ArrowCss variant={ menuCart === true}/> 
-            <ScrollCSS>
-                {
-                cartItem && cartItem.length > 0 ?
-                cartItem.map((item, index) => {
-                    return(
-                    <ItemCartCSS>
-                        <h1> <h2> {index + 1}# </h2> {item.name} </h1>
-                        <p>  {item.price.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}
-                        {       
-                        <>    
+  };
+  return (
+    <>
+      <HeaderButtonCartCss onClick={() => MenuCart()}>
+        <QuantityCartCss>
+          {" "}
+          {cartPriceSum > 0 ? cartItemSum : 0}{" "}
+        </QuantityCartCss>
+        <TiShoppingCart />
+        <CartTotalPriceCss>
+          {" "}
+          {(cartPriceSum &&
+            cartPriceSum.toLocaleString("pt-br", {
+              style: "currency",
+              currency: "BRL",
+            })) ||
+            0}{" "}
+        </CartTotalPriceCss>
+      </HeaderButtonCartCss>
+      <MenuCartCss variant={menuCart === true}>
+        <ArrowCss variant={menuCart === true} />
+        <ScrollCSS>
+          {cartItem && cartItem.length > 0 ? (
+            cartItem.map((item, index) => {
+              return (
+                <ItemCartCSS>
+                  <h1>
+                    {" "}
+                    <h2> {index + 1}# </h2> {item.name}{" "}
+                  </h1>
+                  <p>
+                    {" "}
+                    {item.price.toLocaleString("pt-br", {
+                      style: "currency",
+                      currency: "BRL",
+                    })}
+                    {
+                      <>
                         <ContainerGridCss>
-                            <ButtonPutCSS>
-                                <button onClick={() => PutQuantity(item.id_product )}> <MdAddShoppingCart/> </button>  
-                                </ButtonPutCSS>                                       
-                                <InputCss> 
-                                    <input
-                                        type={"number"}
-                                        placeholder={UserItemQuantity(item.id_product) }
-                                        name={"quantity"}
-                                        onChange={onChange}
-                                        required
-                                        min="1"
-                                        value={formValues.name}
-                                        ></input>
-                                    </InputCss>
-                                    <ButtonDeleteCSS> 
-                                    <button onClick={() => DeleteProduct(item.id_product)}> <MdRemoveShoppingCart/> </button> 
-                                </ButtonDeleteCSS>
-                        </ContainerGridCss>      
-                            </>   
-                        }     
-                        </p>
-                    </ItemCartCSS> )
-                } ) 
-                    :
-                    <EmptyCartCSS> 
-                        <img src='https://programada.shopper.com.br/static/img/apple-icon.png' width={100}></img> 
-                        <h1> Carrinho Vazio </h1> 
-                        <p> Adicione algum item ao Carrinho :) </p> 
-                    </EmptyCartCSS>
-                } 
-            </ScrollCSS>  
-            <FinaliteBuy variant={ menuCart === true} onClick={() => {FinalizePurchase()}} > FINALIZAR COMPRA </FinaliteBuy> 
-            </MenuCartCss>
-        </>
-        )
-}
+                          <ButtonPutCSS>
+                            <button
+                              onClick={() => PutQuantity(item.id_product)}
+                            >
+                              {" "}
+                              <MdAddShoppingCart />{" "}
+                            </button>
+                          </ButtonPutCSS>
+                          <InputCss>
+                            <input
+                              type={"number"}
+                              placeholder={UserItemQuantity(item.id_product)}
+                              name={"quantity"}
+                              onChange={onChange}
+                              required
+                              min="1"
+                              value={formValues.name}
+                            ></input>
+                          </InputCss>
+                          <ButtonDeleteCSS>
+                            <button
+                              onClick={() => DeleteProduct(item.id_product)}
+                            >
+                              {" "}
+                              <MdRemoveShoppingCart />{" "}
+                            </button>
+                          </ButtonDeleteCSS>
+                        </ContainerGridCss>
+                      </>
+                    }
+                  </p>
+                </ItemCartCSS>
+              );
+            })
+          ) : (
+            <EmptyCartCSS>
+              <img
+                src="https://programada.shopper.com.br/static/img/apple-icon.png"
+                width={100}
+              ></img>
+              <h1> Carrinho Vazio </h1>
+              <p> Adicione algum item ao Carrinho :) </p>
+            </EmptyCartCSS>
+          )}
+        </ScrollCSS>
+        <FinaliteBuy
+          variant={menuCart === true}
+          onClick={() => {
+            FinalizePurchase();
+          }}
+        >
+          {" "}
+          FINALIZAR COMPRA{" "}
+        </FinaliteBuy>
+      </MenuCartCss>
+    </>
+  );
+};
